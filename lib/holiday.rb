75280b00b7bbd7a9ceedb697fcd26592a269facf
Tyler Hunt <tyhunt04@gmail.com>
2019-02-20 18:25:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holidays|
+  holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |name, value|
       holiday_name = name.to_s.split("_")
